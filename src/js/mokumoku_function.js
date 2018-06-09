33ebc2678dd7c54fe5dcba2db5967e73a4430b61
k5jp1015 <k5.magami@gmail.com>
2018-06-09 14:34:44 +0900

    async/await関数を作成

--- src/js/mokumoku_function.js
+++ src/js/mokumoku_function.js
@@ -4,5 +4,34 @@ const helloFunc = (target) => {
     return "hello," + target
 };
 
+function sampleResolve(value) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(value);
+        }, 2000);
+    })
+}
+
+function sampleResolve2(value) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(value * 2);
+        }, 1000);
+    })
+}
+
+async function sample() {
+    const [a, b] = await Promise.all([sampleResolve(5), sampleResolve(10)]);
+    const c = await sampleResolve2(b);
+
+    return [a, b, c];
+}
+
+sample().then(([a, b, c]) => {
+    console.log(a, b, c); // => 5 10 20
+});
+
 // モジュールを外出しする場合はexportで指定する
 export {helloFunc}
+
+// このjsをnodeで実効する場合は`node --require babel-register  src/js/mokumoku_function.js`のようにbabel-registerを拡張機能として使う必要がある
